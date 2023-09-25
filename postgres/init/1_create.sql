18d4bfd7ff5531906555b9e5fbe0290429955f3e
K <klen9211@gmail.com>
2023-09-25 23:18:45 +0900

    テーブル変更対応

--- postgres/init/1_create.sql
+++ postgres/init/1_create.sql
@@ -1,4 +1,9 @@
 /* DATABASEを作成 */
-CREATE DATABASE "Implem.Pleasanter";
-\c "Implem.Pleasanter"
-create extension pg_trgm;
+create user "Implem.Pleasanter_Owner" with password 'SetAdminsPWD';
+create user "Implem.Pleasanter_User" with password 'SetUsersPWD';
+create schema authorization "Implem.Pleasanter_Owner";
+
+/* DATABASEを作成 */
+create database "Implem.Pleasanter" with owner "Implem.Pleasanter_Owner";
+\c "Implem.Pleasanter";
+CREATE EXTENSION IF NOT EXISTS pg_trgm;
